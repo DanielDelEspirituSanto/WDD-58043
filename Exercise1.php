6ae20d174713f6974068b9803e613e8a4bf88b0c
Daniel Del Espiritu Santo <69961212+DanielDelEspirituSanto@users.noreply.github.com>
2022-09-30 16:11:01 +0800

    Add files via upload

---
+++ Exercise1.php
@@ -0,0 +1,28 @@
+<html>
+<head><title> Exercise 1</title></head>
+<body bgcolor = pink>
+<h2>Adamson University</h2> 
+<?php echo " ";
+
+	$Fullname = "Daniel Gutierrez Del Espiritu Santo ";
+	echo "Full name: "; echo "<b>","<u>" . $Fullname  ."</b>","</u>";
+	
+	echo "<br>"; echo "<br>";
+	
+	$CollegeProgram = "Computer Engineering"; 
+	echo "College Program: "; echo "<b>","<u>" . $CollegeProgram ."</b>","</u>";
+	
+	echo "<br>"; echo "<br>";
+	
+	$YearandSection = "3rd year CPE - 301";
+	echo "Year and Section: "; echo "<b>","<u>" . $YearandSection ."</b>","</u>";
+
+	echo "<br>"; echo "<br>";
+		
+	$string = "Today, I will learn PHP Programming";
+	echo "<center>" . $string . "</center>";
+
+?>
+
+</body>
+</html>
