29cc3dba1a2216dcd27e93442898cc2ac38fcc53
Daniel Del Espiritu Santo <69961212+DanielDelEspirituSanto@users.noreply.github.com>
2022-10-21 09:36:23 +0800

    Add files via upload

---
+++ demo4.php
@@ -0,0 +1,55 @@
+<html>
+<head><title> Looping Statements  </title></head>
+<body>
+<?php
+
+/*$fuel = 10;
+//while loop
+while($fuel >1)
+{
+	echo "There's enough fuel <br>"; // break; for 1 iteration
+	--$fuel;
+} 
+
+do 
+{
+	echo "There's enough fuel". $fuel. "<br>";
+}
+while (--$fuel >=1);
+
+
+$count = 1;
+
+do
+	echo "$count times 12 is: :". $count*12 . "<br>";
+while(++$count <=12); 
+
+ //for loop
+for($fuel=10; $fuel>=1; --$fuel)
+{
+	echo "There's enough fuel". "<br>";
+}
+
+$colors = array("red", "green", "blue");
+foreach($colors as $value)
+	echo $value. "<br>"; 
+	
+for($x=1;$x<10;++$x)
+{	if($x==4)
+	{
+		continue;
+	}
+	echo" The number is: $x"." <br>";
+} */
+
+//Application 2
+
+for($x=3;$x<=10;$x++)
+{
+	if($x<=9)
+	echo "Hello $x". "<br>";
+}
+
+?>
+</body>
+</html>
